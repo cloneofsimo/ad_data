22af14fdd9f307fb2128b55dfe0527198ec1f598
SimoRyu <cloneofsimo@gmail.com>
2020-06-17 15:25:24 +0900

    rebuilt

--- execute_transformation.bat
+++ execute_transformation.bat
@@ -1,1 +1,1 @@
-C:/Users/clone/Anaconda3/envs/BITCOIN/python.exe c:/Users/clone/Documents/2020spring/perstyle/style_transfer_application/ad_result/evaluate.py --in_path ./clients/bmw --out_path ./result --checkpoint_dir ./checkpoints/chk_3
+C:/Users/clone/Anaconda3/envs/BITCOIN/python.exe c:/Users/clone/Documents/2020spring/perstyle/style_transfer_application/ad_result/evaluate.py --in_path ./clients/dior --out_path ./result --checkpoint_dir ./checkpoints/chk_1
